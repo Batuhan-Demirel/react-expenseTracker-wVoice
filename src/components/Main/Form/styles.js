ab1ce8c7470b3baf513a73ebd25c2691ee8d401c
Batuhan-Demirel <77571726+Batuhan-Demirel@users.noreply.github.com>
2021-04-07 20:58:12 +0300

    Add files via upload

---
+++ src/components/Main/Form/styles.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+export default makeStyles(() => ({
+radioGroup: {
+display: 'flex',
+justifyContent: 'center',
+marginBottom: '-10px',
+},
+button: {
+marginTop: '20px',
+},
+button2: {
+    margin:"auto",
+    marginTop:"20px",
+    borderRadius:"50%",
+    height:"100px",
+    width:"100px",
+},
+button3: {
+    fontSize:"18px",
+    margin:"auto",
+    marginTop:"20px",
+    borderRadius:"50%",
+    height:"130px",
+    width:"130px"
+}
+}));
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { Provider } from "./context/context";
+
+import { SpeechProvider } from "@speechly/react-client";
+
+ReactDOM.render(
+  <SpeechProvider appId={process.env.APP_ID} language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById("root")
+);
